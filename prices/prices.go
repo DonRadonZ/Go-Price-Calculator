24eaeff1c8447412100090bb88ed17de833cf11a
RadonZ <supachai27102000@gmail.com>
2024-04-15 23:03:23 +0700

    add write json

--- prices/prices.go
+++ prices/prices.go
@@ -1,59 +1,39 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	
+	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
 	Prices            []float64
 	TaxRate           float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
-func (job TaxIncludedPriceJob) LoadPrice() {
-	file,err := os.Open("prices.txt")
+func (job *TaxIncludedPriceJob) LoadPrice() {
+
+	lines, err := filemanager.ReadLines("prices.txt")
 
 	if err != nil {
-		fmt.Println("Could not open file!")
 		fmt.Println(err)
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-	
-	for scanner.Scan() {
-	  lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-
-	if err != nil {
-		fmt.Println("Could not open file!")
-		fmt.Println(err)
-		file.Close()
-		return
-	}
-
-	prices := make([]float64, len(lines))
-
-	for lineIndex, line := range lines {
-	  floatPrice, err := strconv.ParseFloat(line, 64)
-
-	  if err != nil {
-		fmt.Println("Converting price to float failed.")
-		fmt.Println(err)
-		file.Close()
 		return
 	  }
 
-	  prices[lineIndex] = floatPrice
-	}
+
+	prices, err := conversion.StringsToFloats(lines)
+
+
+
+	  if err != nil {
+		fmt.Println(err)
+		return
+	  }
 
   job.Prices = prices
+  
 }
 
 func (job *TaxIncludedPriceJob) Process() {
@@ -65,7 +45,10 @@ func (job *TaxIncludedPriceJob) Process() {
 		taxIncludedPrice := price * (1 + job.TaxRate)
 		result[fmt.Sprintf("%.2f",price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
-	fmt.Println(result)
+	
+	job.TaxIncludedPrices = result
+	
+	filemanager.WriteJSON(fmt.Sprintf("result_%0f.json", job.TaxRate*100), job)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
